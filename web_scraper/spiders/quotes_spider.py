2aaa3c8e79bcf62a1fc410c093439474ab6336b4
Joseph Moussa <joeph465@gmail.com>
2021-03-17 21:05:54 -0400

    format quotes scraper

--- web_scraper/spiders/quotes_spider.py
+++ web_scraper/spiders/quotes_spider.py
@@ -1,20 +1,28 @@
 import scrapy
 
 
+def convertToString(encodedString):
+    return encodedString.encode("utf-8").decode("unicode_escape").encode("ascii", "ignore")
+
+
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
     def start_requests(self):
-        urls = [
-            "http://quotes.toscrape.com/page/1/",
-            "http://quotes.toscrape.com/page/2/",
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        url = "http://quotes.toscrape.com/"
+        tag = getattr(self, "tag", None)
+        if tag is not None:
+            url = url + "tag/" + tag
+        yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"quotes-{page}.html"
-        with open(filename, "wb") as f:
-            f.write(response.body)
-        self.log(f"Saved file {filename}")
+        for quote in response.css("div.quote"):
+            string_text = convertToString(quote.css("span.text::text").extract_first()).decode("utf-8")
+            yield {
+                "text": string_text,
+                "author": quote.css("small.author::text").get(),
+            }
+
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
